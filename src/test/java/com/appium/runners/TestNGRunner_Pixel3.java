9629b4aea6da4c074b9d3f71ce36c7ed4bb87171
RobertoTeresa <rbttm1999@gmail.com>
2022-11-10 13:58:33 +0100

    Primer commit

--- src/test/java/com/appium/runners/TestNGRunner_Pixel3.java
+++ src/test/java/com/appium/runners/TestNGRunner_Pixel3.java
@@ -5,7 +5,7 @@ package com.appium.runners;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(plugin = { "pretty", "html:target_TestNG/Pixel4API30/cucumber-report.html", "summary","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, features = {
-		"src/test/resources/features/frontpage.feature","src/test/resources/features/article.feature"}, glue = {
+		"src/test/resources/features/frontpage.feature"}, glue = {
 		"com.appium.stepDefinitions" }, dryRun = false)
 public class TestNGRunner_Pixel3 extends RunnerBase {
 }
